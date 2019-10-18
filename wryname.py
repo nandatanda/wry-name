9bdf26e295fc1cd7b8abc6b139fa9fa1e9cc51ef
nandatanda <39239764+nandatanda@users.noreply.github.com>
2019-10-18 15:29:40 -0400

    Title Preview
    Add modified event to title entry box
    Add modified event to padding radioboxes
    Add preview label and logic to maintain UI integrity

--- wryname.py
+++ wryname.py
@@ -94,6 +94,11 @@ def clear(listbox):
     listbox.delete(0, listbox.size() - 1)
     return
 
+def clear_text(entry):
+    entry.delete('0', tk.END)
+    return
+
+
 
 def debug():
     print("\n**************** DEBUG INFO ***************")
@@ -181,6 +186,23 @@ def apply_filters_button_click():
     select_item(episodes_listbox, latest_selection)
     return
 
+def series_title_changed(series_title):
+    fill = padding_selection.get()
+    title = series_title.get()
+    new_name = title + " - {}.mkv".format("1".zfill(fill))
+    if not title == "":
+        preview_label.config(text=new_name)
+    else:
+        preview_label.config(text="(^~^;)/")
+
+def padding_selection_changed(padding_selection):
+    fill = padding_selection.get()
+    title = series_title.get()
+    new_name = title + " - {}.mkv".format("1".zfill(fill))
+    if not title == "" and not title == "Enter a new title here...":
+        preview_label.config(text=new_name)
+    else:
+        preview_label.config(text="(^~^;)/")
 
 def rename_button_click():
     # The main stuffs will happen here.
@@ -203,7 +225,8 @@ removed_files = []
 formats = {
     ".mkv" : False, ".mp4" : False,
     ".mov" : False, ".flv" : False}
-padding_selection = tk.IntVar(value=0)
+padding_selection = tk.IntVar(value=2)
+series_title = tk.StringVar(value="")
 
 # FRAMES
 menu_frame = tk.Frame(root, bg="light blue")
@@ -222,6 +245,8 @@ padding_label = tk.Label(body_frame, bg=background_color, text="Zero padding:")
 padding_label.place(relx=.7, rely=.4, relwidth=.3, relheight=.1)
 series_title_label = tk.Label(body_frame, bg=background_color, text="Series title:")
 series_title_label.place(relx=.7, rely=.7, relwidth=.3, relheight=.1)
+preview_label = tk.Label(body_frame, bg=background_color, text="(^~^;)/")
+preview_label.place(relx=.7, rely=.85, relwidth=.3, relheight=.1)
 
 
 # BUTTONS
@@ -248,10 +273,12 @@ mov_checkbox.place(relx=.85, rely=.1, relwidth=.15, relheight=.05)
 flv_checkbox = tk.Checkbutton(body_frame, bg=background_color, text=".flv", command=flv_checkbox_click)
 flv_checkbox.place(relx=.85, rely=.2, relwidth=.15, relheight=.05)
 
-# TEXTBOXES
-series_title_textbox = tk.Text(body_frame, height=1)
-series_title_textbox.place(relx=.72, rely=.8, relwidth=.25)
-series_title_textbox.insert(tk.INSERT, "Enter a new title here...")
+# ENTRY
+series_title_entry = tk.Entry(body_frame, textvariable=series_title)
+series_title_entry.place(relx=.72, rely=.8, relwidth=.25)
+series_title_entry.config(justify="center")
+series_title_entry.insert(tk.INSERT, "Enter a new title here...")
+series_title.trace_add("write", lambda name, index, mode = series_title: series_title_changed(series_title))
 
 # RADIOBUTTONS
 one_digit_radiobutton = tk.Radiobutton(body_frame, bg=background_color, var=padding_selection, value=0, text="No padding")
@@ -262,6 +289,7 @@ three_digits_radiobutton = tk.Radiobutton(body_frame, bg=background_color, var=p
 three_digits_radiobutton.place(relx=.70, rely=.6, relwidth=.15, relheight=.05)
 four_digits_radiobutton = tk.Radiobutton(body_frame, bg=background_color, var=padding_selection, value=4, text="Four digits")
 four_digits_radiobutton.place(relx=.85, rely=.6, relwidth=.15, relheight=.05)
+padding_selection.trace_add("write", lambda name, index, mode = padding_selection: padding_selection_changed(padding_selection))
 
 # LISTBOXES
 episodes_listbox = tk.Listbox(body_frame, relief=tk.GROOVE)
